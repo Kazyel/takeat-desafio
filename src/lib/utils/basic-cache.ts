cc1068c377fcd83da13c722775850f9200d2b533
Kazyel <m.mascarelo@gmail.com>
2026-02-06 16:55:52 -0300

    feat: added basic caching to prevent hitting gemini too much

---
+++ src/lib/utils/basic-cache.ts
@@ -0,0 +1,41 @@
+import type { APIClassifyResponse } from "@/lib/types/api";
+
+export class BasicCache {
+	private cache: Map<string, APIClassifyResponse>;
+
+	constructor() {
+		this.cache = new Map<string, APIClassifyResponse>();
+	}
+
+	has(key: string): boolean {
+		return this.cache.has(key);
+	}
+
+	get(key: string): APIClassifyResponse | null {
+		const cachedValue = this.cache.get(key);
+		if (cachedValue) {
+			return cachedValue;
+		}
+		return null;
+	}
+
+	set(key: string, value: APIClassifyResponse): void {
+		this.cache.set(key, value);
+	}
+
+	delete(key: string): void {
+		this.cache.delete(key);
+	}
+
+	clear(): void {
+		this.cache.clear();
+	}
+
+	size(): number {
+		return this.cache.size;
+	}
+
+	generateKey(message: string): string {
+		return message.trim().toLowerCase();
+	}
+}
