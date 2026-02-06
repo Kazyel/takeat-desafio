106d9518bdfd2a514cf67d21bdd12fd58bf27485
Kazyel <m.mascarelo@gmail.com>
2026-02-06 16:55:52 -0300

    feat: added basic caching to prevent hitting gemini too much

--- src/services/classify-service.ts
+++ src/services/classify-service.ts
@@ -6,11 +6,15 @@ import {
 } from "@/lib/helpers/gemini-helpers";
 import type { APIClassifyResponse } from "@/lib/types/api";
 import type { MessageContext } from "@/lib/types/generic";
+import { BasicCache } from "@/lib/utils/basic-cache";
 
 type ClassifyOptions = {
 	gemini?: GoogleGenAI;
 	configs?: ReturnType<typeof getGeminiConfig>;
 };
+
+const MAX_CACHE = 500;
+const cache = new BasicCache();
 
 export async function classifyMessage(
 	message: string,
@@ -19,6 +23,19 @@ export async function classifyMessage(
 ): Promise<APIClassifyResponse> {
 	if (typeof message !== "string" || message.trim().length === 0) {
 		throw new Error("Input inválido: mensagem deve ser uma string não vazia");
+	}
+
+	const key = cache.generateKey(message);
+
+	if (cache.has(key)) {
+		console.log("CACHE HIT", key);
+
+		const cachedValue = cache.get(key);
+		if (!cachedValue) {
+			throw new Error("Cached value is null");
+		}
+
+		return cachedValue;
 	}
 
 	try {
@@ -38,7 +55,18 @@ export async function classifyMessage(
 			throw new Error("Gemini retornou uma resposta vazia");
 		}
 
-		return parseGeminiResponse(result.text);
+		const parsedResponse = parseGeminiResponse(result.text);
+		const hasContext = context.length > 0;
+
+		if (!hasContext) {
+			if (cache.size() >= MAX_CACHE) {
+				cache.clear();
+			}
+
+			cache.set(key, parsedResponse);
+		}
+
+		return parsedResponse;
 	} catch (error) {
 		console.error("Erro durante a classificação:", error);
 		throw new Error(`Falha ao classificar mensagem: ${error}`);
